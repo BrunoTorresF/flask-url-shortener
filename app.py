8caba27051c1bc846cc56e7663d14903af18b895
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-07-02 20:38:32 -0500

    add session and template blocks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -9,7 +9,7 @@ app.secret_key = 'hsddkukhnsdkfosyd'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
@@ -36,6 +36,7 @@ def your_url():
 
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
+            session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
@@ -52,3 +53,13 @@ def redirect_to_url(code):
                 else:
                     return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
     return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
+
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
